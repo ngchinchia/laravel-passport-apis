d3fd387fb9e0c419884ebb751e91915076e729b3
Ng Chin Chia <billyngchinchia@gmail.com>
2023-12-05 10:36:17 +0800

    add new controller & laravel passport setup

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Register API (POST)
+    public function register(Request $request){
+
+    }
+    // Login API (POST)
+    public function login(Request $request){
+
+    }
+
+    // Profile API (POST)
+    public function profile(){
+
+    }
+
+    // Logout API (POST)
+    public function logout(){
+
+    }
+}
